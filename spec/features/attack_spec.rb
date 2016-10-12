8ff434388932c9f9db650b27d4c5a42e9510669d
andy jenkins <andygjenkins@gmail.com>
2016-10-12 17:26:13 +0100

    Finish challenge 26

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 feature 'attacking' do
-  
+
   scenario "attack player 2 reduces player HP by 10" do
     sign_in_and_play
     click_button "Attack"
+    click_link "OK"
     expect(page).to have_content "Mittens HP: 40"
   end
 
