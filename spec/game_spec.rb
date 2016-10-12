31cdb4fbbe4ae0b3caaefc927d794bf0186aa5a7
Glenn <glynester@hotmail.com>
2016-10-12 15:57:22 +0000

    Skinny controller refactor. Tests passed.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,24 @@
 require 'spec_helper'
 
 describe Game do
-  subject(:game) { described_class.new }
-  let(:dave) { double :dave }
-  let(:mittens) { double :mittens }
+
+  subject(:game) { described_class.new(player_1, player_2) }
+  let(:player_1) { double :player_1 }
+  let(:player_2) { double :player_2 }
+
+  describe "initialize" do
+    it "accepts 2 player instances" do
+      
+      expect(game.player_1).to eq player_1
+      
+    end
+    
+  end
 
   describe '#attack' do
     it 'calls take_damage on the opponent' do
-      expect(mittens).to receive(:take_damage)
-      game.attack(mittens)
+      expect(player_2).to receive(:take_damage)
+      game.attack(player_2)
     end
   end
 end
