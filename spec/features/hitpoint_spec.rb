03e4f92fd4aadf0cc94280c20cba8b814ff7552f
andy jenkins <andygjenkins@gmail.com>
2016-10-12 11:41:41 +0100

    DRY out test code and complete ch19

--- spec/features/hitpoint_spec.rb
+++ spec/features/hitpoint_spec.rb
@@ -2,16 +2,8 @@ require 'spec_helper'
 
 feature 'hit points' do
   scenario 'show players hit points' do
-    visit('/')
-    fill_in :player_1_name, with: "John"
-    fill_in :player_2_name, with: "Dave"
-    click_button "Submit"
-    expect(page).to have_content "Dave HP: 50"
+    sign_in_and_play
+    expect(page).to have_content "Mittens HP: 50"
   end
 
 end
-
-
-
-
-
