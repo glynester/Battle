31661e3a5c6e32cf1555a18f36a830a83dd74e22
Darrenf <darrenf54321@gmail.com>
2016-10-13 11:27:12 +0100

    Chap27 complete

--- lib/game.rb
+++ lib/game.rb
@@ -2,8 +2,11 @@
 
 class Game
 
-  def initialize(player_one, player_two)
-    @players = [player_one, player_two]
+  attr_reader :current_turn
+
+  def initialize(player_1, player_2)
+    @players = [player_1, player_2]
+    @current_turn = player_1
   end
 
   def player_1
@@ -16,6 +19,16 @@ class Game
 
   def attack(player)
     player.take_damage
+  end
+
+  def switch_turns
+    @current_turn = opponent_of(current_turn)
+  end
+
+  private
+
+  def opponent_of(the_player)
+    @players.select {|player| player != the_player }.first
   end
 
 end
--- spec/features/switch_turns_spec.rb
+++ spec/features/switch_turns_spec.rb
@@ -3,12 +3,22 @@ feature 'Switch turns' do
     sign_in_and_play
     expect(page).to have_content "Dave's turn"
   end
-  
+
   scenario "After player 1 has had their turn , it should be the turn of player 2" do
     sign_in_and_play
     click_button "Attack"
     click_link "OK"
     expect(page).not_to have_content "Dave's turn"
     expect(page).to have_content "Mittens's turn"
+  end
+
+    scenario "After player 2 has had their turn , it should be the turn of player 1" do
+    sign_in_and_play
+    click_button "Attack"
+    click_link "OK"
+    click_button "Attack"
+    click_link "OK"
+    expect(page).not_to have_content "Mitten's turn"
+    expect(page).to have_content "Dave's turn"
   end
 end
