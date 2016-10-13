5a9860365689d2bdf06f3a7e407d46ea6de78458
Glenn <glynester@hotmail.com>
2016-10-13 09:42:00 +0000

    Step 27 Capybara tests written.

---
+++ spec/features/switch_turns_spec.rb
@@ -0,0 +1,14 @@
+feature 'Switch turns' do
+  scenario "Show next player's turn at the start of the game" do
+    sign_in_and_play
+    expect(page).to have_content "Dave's turn"
+  end
+  
+  scenario "After player 1 has had their turn , it should be the turn of player 2" do
+    sign_in_and_play
+    click_button "Attack"
+    click_link "OK"
+    expect(page).not_to have_content "Dave's turn"
+    expect(page).to have_content "Mittens's turn"
+  end
+end
