629afcc0fd73836a322b30555d83466febb2aefe
Glenn <glynester@hotmail.com>
2016-10-14 08:31:16 +0000

    Minor changes.

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require './lib/game'
 class Battle < Sinatra::Base
 
   set :session_secret, "My session secret"
-
+  
   get '/' do
     erb :index
   end
@@ -14,7 +14,7 @@ class Battle < Sinatra::Base
   post '/names' do
     player_1 = Player.new(params[:player_1_name])
     player_2 = Player.new(params[:player_2_name])
-    $game = Game.create(player_1, player_2)
+    @game = Game.create(player_1, player_2)
     redirect '/play'
   end
   
@@ -23,14 +23,12 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-    #@game = $game
     erb :play
   end
 
   post '/attack' do
-    #@game = $game
     @game.attack(@game.opponent_of(@game.current_turn))
-    if $game.game_over?         #$game
+    if @game.game_over?         #$game
       redirect '/game-over' 
     else
       redirect '/attack' 
@@ -38,17 +36,15 @@ class Battle < Sinatra::Base
   end
   
   get '/attack' do
-    #@game = $game
     erb :attack
   end
 
   post '/switch-turns' do
-    $game.switch_turns
+    @game.switch_turns
     redirect '/play'
   end
   
   get '/game-over' do
-    #@game = $game
     erb :game_over
   end
 
