8fd5713e1d7e63f3aa97b249f94d97dbcbf46f88
Glenn <glynester@hotmail.com>
2016-10-14 14:25:02 +0000

    Step 27 tests and functionality done.

--- spec/features/switch_turns_spec.rb
+++ spec/features/switch_turns_spec.rb
@@ -3,6 +3,7 @@ feature 'switch turns' do
 	scenario 'will show player1 turn' do
 		sign_in_and_play
 		expect(page).to have_content('Toms turn')
+	end	
 
     scenario 'will show player2 turn' do
       sign_in_and_play
@@ -11,5 +12,4 @@ feature 'switch turns' do
       expect(page).not_to have_content('Toms turn')
       expect(page).to have_content('Jerrys turn')
     end
-	end
 end
